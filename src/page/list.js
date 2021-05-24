705c7252c03b9c60ec58ac47ad2bb3ae5c478d88
wilson kao <wilkao036@gmail.com>
2021-05-24 14:56:24 +0800

    增加loading 效果

--- src/page/list.js
+++ src/page/list.js
@@ -1,8 +1,26 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { from, fromEvent, of } from 'rxjs';
 import { mergeMap, finalize, catchError } from 'rxjs/operators';
 import styled from 'styled-components';
+import { Spin } from 'antd';
+
+const StyledH1 = styled.h1`
+  text-align: center;
+  font-size: 48px;
+  font-weight: 800;
+  @media (max-width: 700px) {
+    font-size: 36px;
+  }
+  @media (max-width: 320px) {
+    font-size: 28px;
+  }
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+`;
 
 const StyledUl = styled.ul`
   display: flex;
@@ -51,6 +69,7 @@ export default function List() {
   const [data, setData] = useState([]);
   const [offset, setOffset] = useState(0);
   const [isbottom, setBottom] = useState(false);
+  const hasNext = useRef(true);
   const { pathname } = useLocation();
 
   const url = useMemo(
@@ -65,7 +84,7 @@ export default function List() {
         Math.ceil(window.innerHeight + window.scrollY) >=
         document.documentElement.scrollHeight;
 
-      if (bottom) {
+      if (bottom && hasNext.current) {
         setBottom(true);
       }
     });
@@ -93,28 +112,39 @@ export default function List() {
       )
       .subscribe(({ assets }) => {
         setData(data.concat(assets));
+        if (assets.length < 20) {
+          hasNext.current = false;
+        }
       });
     return () => fetchData$.unsubscribe();
   }, [url]);
 
   return (
-    <StyledUl>
-      {data.map(({ id, token_id, image_url, name, asset_contract }) => (
-        <li key={id}>
-          <Link
-            to={{
-              pathname: `/detail${token_id}`,
-              state: {
-                token_id,
-                asset_contract,
-              },
-            }}
-          >
-            <img src={image_url} alt={name} />
-          </Link>
-          <p>{name}</p>
-        </li>
-      ))}
-    </StyledUl>
+    <>
+      <StyledH1>List</StyledH1>
+      <StyledUl>
+        {data.map(({ id, token_id, image_url, name, asset_contract }) => (
+          <li key={id}>
+            <Link
+              to={{
+                pathname: `/detail${token_id}`,
+                state: {
+                  token_id,
+                  asset_contract,
+                },
+              }}
+            >
+              <img src={image_url} alt={name} />
+            </Link>
+            <p>{name}</p>
+          </li>
+        ))}
+      </StyledUl>
+      {isbottom && (
+        <StyledDiv>
+          <Spin tip="Loading..." />
+        </StyledDiv>
+      )}
+    </>
   );
 }
